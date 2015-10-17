628f536c14e598dec9d06eb6d0f6224353474105
mitogh <mitogh@gmail.com>
2015-10-17 10:40:18 -0500

    Add new filters to work on page templates

--- class-katana.php
+++ class-katana.php
@@ -43,10 +43,12 @@ if ( ! class_exists( 'Katana' ) ) {
 		 * WP default filter that runs before the images are being generated, then
 		 * appplies the custom filter from Katana.
 		 *
-		 * @url http://codex.wordpress.org/Plugin_API/Filter_Reference/intermediate_image_sizes.html
+		 * @since 1.0.0
+		 *
+		 * @lik http://codex.wordpress.org/Plugin_API/Filter_Reference/intermediate_image_sizes.html
+		 *
 		 * @param array $sizes The register sizes of images in WP.
 		 * @return array $sizes The array of sizes
-		 * @since 1.0.0
 		 */
 		public function filter( $sizes ) {
 			return apply_filters( KATANA_FILTER, $sizes, $this->get_the_id() );
@@ -55,18 +57,19 @@ if ( ! class_exists( 'Katana' ) ) {
 		/**
 		 * Allow access to the current post_id using the $_REQUEST variable
 		 *
+		 * @since 1.0.0
+		 *
 		 * @return int Return a int from 0 to n, that represents the current post id
 		 */
 		public function get_the_id() {
-			if ( isset( $_REQUEST['post_id'] ) && absint( $_REQUEST['post_id'] ) === 0 ) {
-				$a = 10;
-			}
-			return 0;
+			return isset( $_REQUEST['post_id'] ) ?  absint( $_REQUEST['post_id'] ) : 0;
 		}
 
 		/**
 		 * Custom filter that applies filters for custom post types and using only
 		 * the ID of the post
+		 *
+		 * @since 1.0.0
 		 *
 		 * @param array $sizes The images sizes.
 		 * @param int   $ID The id of the post, page or custom post type.
@@ -78,12 +81,15 @@ if ( ! class_exists( 'Katana' ) ) {
 			}
 			$sizes = $this->post_id_filter( $sizes, $ID );
 			$sizes = $this->post_type_filter( $sizes, $ID );
+			$sizes = $this->page_template_filter( $sizes, $ID );
 			return $sizes;
 		}
 
 		/**
 		 * Creates a filter based on the post_id like: katana_refine_%d
 		 * where %d is any post_id.
+		 *
+		 * @since 1.0.0
 		 *
 		 * @param array $sizes The images sizes.
 		 * @param int   $ID The id of the post, page or custom post type.
@@ -98,17 +104,74 @@ if ( ! class_exists( 'Katana' ) ) {
 		 * Creates a filter based on the post_type like: katana_refine_%s
 		 * where %s is the post type can be: 'post', 'page' or a custom one.
 		 *
+		 * @since 1.0.0
+		 *
 		 * @param array $sizes The images sizes.
 		 * @param int   $ID The id of the post, page or custom post type.
 		 * @return return the array with the new sizes.
 		 */
 		public function post_type_filter( $sizes, $ID ) {
 			$type = get_post_type( $ID );
-			if ( '' !== $type ) {
-				$filter_name = sprintf( '%s_%s', KATANA_FILTER, $type );
-				$sizes = apply_filters( $filter_name, $sizes );
+			if ( empty( $type ) ) {
+				return $sizes;
 			}
-			return $sizes;
+			$filter_name = sprintf( '%s_%s', KATANA_FILTER, $type );
+			return apply_filters( $filter_name, $sizes );
+		}
+
+		/**
+		 * Filter that allow to change the sizes on pages that uses custom
+		 * page templates.
+		 *
+		 * @since 1.1.0
+		 *
+		 * @param array $sizes The images sizes.
+		 * @param int   $ID The id of the post, page or custom post type.
+		 * @return return the array with the new sizes.
+		 */
+		public function page_template_filter( $sizes, $ID ) {
+			$template = get_page_template_slug( $ID );
+			if ( empty( $template ) ) {
+				return $sizes;
+			}
+			$suffix_name = $this->create_filter_name( $template );
+			$filter_name = sprintf( '%s_%s', KATANA_FILTER, $suffix_name );
+			return apply_filters( $filter_name, $sizes );
+		}
+
+		/**
+		 * Helps by creating a more meaningful name on the template by changing
+		 * the default page template path for example something like:
+		 * page-templates/awesome.php becomes: page_templates_awesome
+		 *
+		 * @since 1.1.0
+		 *
+		 * @param string $original The original path of the template.
+		 * @return string Formated template name as filter name
+		 */
+		private function create_filter_name( $original ) {
+			$change = array( '-', '/' );
+			$original = str_replace( $change, '_', $original );
+			return $this->remove_type( $original );
+		}
+
+		/**
+		 * Remove the .php type of the file from the string and returns only
+		 * the name of the file without the extension
+		 *
+		 * @since 1.1.0
+		 *
+		 * @param string $file The file name.
+		 * @return string $result The file name without the .php extension
+		 */
+		private function remove_type( $file ) {
+			$pattern = '/\.php/i';
+			$matches = preg_split( $pattern, $file );
+			$result = '';
+			if ( count( $matches ) > 0 ) {
+				$result = $matches[0];
+			}
+			return $result;
 		}
 	}
 }
